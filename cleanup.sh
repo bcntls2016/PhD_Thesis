d13dc0b0a251f7c52604465ba5745fd11b5045d3
François Coppens <francois.coppens@irsamc.ups-tlse.fr>
2018-05-24 17:23:10 +0200

    Added the correct french summary by Nadine. Repharsed and extended the text in Chapter 7 and added more emphasis to the fact that the work done by us was inspired and motivated by experiments of Gomez et al.

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,7 @@
 
 for DIR in $(find . -not -path '*/\.*' -type d)
 do
-	rm -fv $DIR/*.{aux,log,out,toc,bbl,blg,synctex.gz} 2> /dev/null
+	rm -fv $DIR/*.{aux,log,lof,lot,out,toc,bbl,blg,synctex.gz} 2> /dev/null
 done
 
 find . -name '.DS_Store' -exec rm -v {} 2> /dev/null \;
